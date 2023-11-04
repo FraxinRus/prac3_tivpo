3d0b40924b5a68e45d94cef1539f4cdab26bd2f8
FraxinRus <redrowan.03@mail.ru>
2023-11-04 16:16:25 +0300

    Bug fixed
    Исправлена ошибка

--- currency_converter.py
+++ currency_converter.py
@@ -1,11 +1,11 @@
 def currency_converter(amount, from_currency, to_currency):
     if from_currency == 'USD' and to_currency == 'RUB':  # USD -> RUB
-        return amount * 92.7325
+        return round(round(amount, 2) * 92.7325, 2)
     elif from_currency == 'USD' and to_currency == 'EUR':  # USD -> EUR
-        return amount * 0.947
+        return round(round(amount, 2) * 0.947, 2)
     elif from_currency == 'USD' and to_currency == 'VND':  # USD -> VND
-        return amount * 24580
+        return round(round(amount, 2) * 24580, 2)
     elif from_currency == 'RUB' and to_currency == 'VND':  # RUB -> VND
-        return amount * 2.6545417
+        return round(round(amount, 2) * 265.46, 2)
     else:
         return 'Invalid input'
