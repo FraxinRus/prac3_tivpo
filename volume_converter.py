8e459d7273a6c473358d0dd395aa982d903dd579
FraxinRus <redrowan.03@mail.ru>
2023-11-04 16:37:19 +0300

    Bug fixed
    Исправлена ошибка

--- volume_converter.py
+++ volume_converter.py
@@ -4,16 +4,16 @@
 
 def volume_converter(value, from_m_system, to_m_system):
     if from_m_system == 1 and to_m_system == 2:
-        return value * 0.26417205
+        return round(round(value, 2) * 0.26417205, 2)
     elif from_m_system == 1 and to_m_system == 3:
-        return value * 0.81307423368
+        return round(round(value, 2) * 0.81307423368, 2)
     elif from_m_system == 2 and to_m_system == 1:
-        return value * 3.78541181779
+        return round(round(value, 2) * 3.78541181779, 2)
     elif from_m_system == 2 and to_m_system == 3:
-        return value * 3.0778208129
+        return round(round(value, 2) * 3.0778208129, 2)
     elif from_m_system == 3 and to_m_system == 1:
-        return value * 1.2299
+        return round(round(value, 2) * 1.2299, 2)
     elif from_m_system == 3 and to_m_system == 2:
-        return value * 0.3249052043
+        return round(round(value, 2) * 0.3249052043, 2)
     else:
-        return value  # Если системы совпадают или ключи некорректны, возвращаем исходное значение
+        return round(round(value, 2), 2)  # Если системы совпадают или ключи некорректны, возвращаем исходное значение
