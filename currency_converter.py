2d24d8ccbb1277ac6829d420b54bb8a635793049
FraxinRus <redrowan.03@mail.ru>
2023-11-04 16:15:51 +0300

    Added function code
    Добавлен код функции перевода валюты

--- currency_converter.py
+++ currency_converter.py
@@ -1,2 +1,11 @@
 def currency_converter(amount, from_currency, to_currency):
-    return amount
+    if from_currency == 'USD' and to_currency == 'RUB':  # USD -> RUB
+        return amount * 92.7325
+    elif from_currency == 'USD' and to_currency == 'EUR':  # USD -> EUR
+        return amount * 0.947
+    elif from_currency == 'USD' and to_currency == 'VND':  # USD -> VND
+        return amount * 24580
+    elif from_currency == 'RUB' and to_currency == 'VND':  # RUB -> VND
+        return amount * 2.6545417
+    else:
+        return 'Invalid input'
