fb0a0388ac94fcbdba1f2b752e1472ed4a0c26a8
FraxinRus <redrowan.03@mail.ru>
2023-11-04 16:36:23 +0300

    Added function code
    Добавлен код функции перевода объема

--- volume_converter.py
+++ volume_converter.py
@@ -3,4 +3,17 @@
 # 3 - Старорусская штофы
 
 def volume_converter(value, from_m_system, to_m_system):
-    return value
+    if from_m_system == 1 and to_m_system == 2:
+        return value * 0.26417205
+    elif from_m_system == 1 and to_m_system == 3:
+        return value * 0.81307423368
+    elif from_m_system == 2 and to_m_system == 1:
+        return value * 3.78541181779
+    elif from_m_system == 2 and to_m_system == 3:
+        return value * 3.0778208129
+    elif from_m_system == 3 and to_m_system == 1:
+        return value * 1.2299
+    elif from_m_system == 3 and to_m_system == 2:
+        return value * 0.3249052043
+    else:
+        return value  # Если системы совпадают или ключи некорректны, возвращаем исходное значение
