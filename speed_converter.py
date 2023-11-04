a4efb547e01fc1f6852eb465a4fd0fcb18be3731
FraxinRus <redrowan.03@mail.ru>
2023-11-04 15:17:50 +0300

    Bug fixed
    Исправлена ошибка

--- speed_converter.py
+++ speed_converter.py
@@ -5,16 +5,16 @@
 # Функция для конвертации скорости
 def speed_converter(value, from_system, to_system):
     if from_system == 0 and to_system == 1:  # км/ч в м/с
-        return value * 1000 / 3600
+        return round(value * 1000 / 3600, 2)
     elif from_system == 0 and to_system == 2:  # км/ч в миль/ч
-        return value * 0.621371
+        return round(value * 0.621371, 2)
     elif from_system == 1 and to_system == 0:  # м/с в км/ч
-        return value * 3600 / 1000
+        return round(value * 3600 / 1000, 2)
     elif from_system == 1 and to_system == 2:  # м/с в миль/ч
-        return value * 2.23694
+        return round(value * 2.23694, 2)
     elif from_system == 2 and to_system == 0:  # миль/ч в км/ч
-        return value * 1.60934
+        return round(value * 1.60934, 2)
     elif from_system == 2 and to_system == 1:  # миль/ч в м/с
-        return value * 0.44704
+        return round(value * 0.44704, 2)
     else:
-        return value
+        return round(value, 2)
